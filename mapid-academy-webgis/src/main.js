56ffa8590f06fc2add7632fb88f4419ca71f1e08
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2026-05-22 14:18:39 +0700

    feat: add simple maplibre webmap

--- mapid-academy-webgis/src/main.js
+++ mapid-academy-webgis/src/main.js
@@ -1,5 +1,20 @@
-const mapidElement = document.createElement('div');
+// const mapidElement = document.createElement('div');
 
-mapidElement.textContent = "Hello from MAPID Academy!";
+// mapidElement.textContent = "Hello from MAPID Academy!";
 
-document.body.appendChild(mapidElement);
+// document.body.appendChild(mapidElement);
+
+// MapLibre components
+import { Map } from 'maplibre-gl';
+
+const mapElement = document.createElement('div');
+mapElement.id = "map";
+mapElement.style.height = "300px"
+document.body.appendChild(mapElement);
+
+const map = new Map({
+    container: 'map',
+    style: 'https://demotiles.maplibre.org/globe.json',
+    center: [0, 0],
+    zoom: 1
+});
