d028d563e4b28039f9888600f0581e7cadef13dd
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2026-05-27 10:28:39 +0700

    feat: manage events in a module and add simple popup

---
+++ mapid-academy-webgis/src/events/monasEvent.js
@@ -0,0 +1,20 @@
+import { Popup } from "maplibre-gl";
+import monasUrl from "../data/monas.geojson?url";
+import { addMonasLayer, addMonasImage } from "../layers/monasLayer";
+
+export function addMonasEvents(map){
+    map.on('load', () => {
+        addMonasLayer("tugu", map);
+        addMonasLayer("kolam", map, monasUrl);
+        addMonasLayer("mancur", map, "https://geoserver.mapid.io/layers_new/get_layer?api_key=9498e99e38f84c558f72f75af447c63b&layer_id=6a15cc319eba37cd77a151ef&project_id=6a15cc13db752242b79ed6d7");
+    
+        addMonasImage("spongebob", map);
+    });
+
+    map.on("click", "mancur-point", function(event){
+        new Popup()
+            .setLngLat(event.lngLat)
+            .setHTML("<div>Hello World!</div>")
+            .addTo(map)
+    })
+};
--- mapid-academy-webgis/src/main.js
+++ mapid-academy-webgis/src/main.js
@@ -1,13 +1,6 @@
-import monasUrl from "./data/monas.geojson?url";
-import { createMonasMap } from "./maps/monasMap"
-import { addMonasLayer, addMonasImage } from "./layers/monasLayer"
+import { createMonasMap } from "./maps/monasMap";
+import { addMonasEvents } from "./events/monasEvent";
 
 const map = createMonasMap();
 
-map.on('load', () => {
-    addMonasLayer("tugu", map);
-    addMonasLayer("kolam", map, monasUrl);
-    addMonasLayer("mancur", map, "https://geoserver.mapid.io/layers_new/get_layer?api_key=9498e99e38f84c558f72f75af447c63b&layer_id=6a15cc319eba37cd77a151ef&project_id=6a15cc13db752242b79ed6d7");
-
-    addMonasImage("spongebob", map);
-});
+addMonasEvents(map);
