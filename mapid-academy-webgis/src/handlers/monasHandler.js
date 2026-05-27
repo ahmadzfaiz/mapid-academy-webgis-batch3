4343d9d25656338b3ec395ef4b3e8a76125a1aac
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2026-05-27 13:47:03 +0700

    feat: add handler in webmap

---
+++ mapid-academy-webgis/src/handlers/monasHandler.js
@@ -0,0 +1,3 @@
+export function addMonasHandler(map) {
+    map.scrollZoom.disable()
+}
--- mapid-academy-webgis/src/main.js
+++ mapid-academy-webgis/src/main.js
@@ -1,8 +1,10 @@
 import { createMonasMap } from "./maps/monasMap";
 import { addMonasEvents } from "./events/monasEvent";
 import { addMonasControl } from "./controls/monasControl";
+import { addMonasHandler } from "./handlers/monasHandler";
 
 const map = createMonasMap();
 
 addMonasEvents(map);
 addMonasControl(map);
+addMonasHandler(map);
