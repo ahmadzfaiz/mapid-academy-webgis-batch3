5572e92324ae92456d40bc97103831cc83dd9bf5
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2026-07-10 21:30:49 +0700

    DRY approach for vector layer

--- webapp-maplibre/src/main.js
+++ webapp-maplibre/src/main.js
@@ -1,6 +1,5 @@
 import { Map } from 'maplibre-gl';
-import naturalEarthData from "./data/ne.geojson?url";
-import areaData from "./data/area.geojson?url"
+import { addKotaLayer, addPulauLayer } from './layers/vector';
 
 const mapElement = document.createElement('div');
 mapElement.id = 'map';
@@ -35,40 +34,8 @@ const map = new Map({
 
 
 map.on("load", () => {
-
-  // Layer Vektor - Titik
-  map.addSource('kota', {
-    type: 'geojson',
-    data: naturalEarthData
-  });
-  
-  map.addLayer({
-    id: "titik-kota",
-    type: "circle",
-    source: "kota",
-    paint: {
-      "circle-radius": 5,
-      "circle-color": "blue",
-      "circle-stroke-width": 1,
-      "circle-stroke-color": "black"
-    }
-  })
-
-  // Layer Vektor - Poligon
-  map.addSource('pulau', {
-    type: "geojson",
-    data: areaData
-  })
-
-  map.addLayer({
-    id: "area-pulau",
-    type: "fill",
-    source: "pulau",
-    paint: {
-      "fill-color": "orange",
-      "fill-outline-color": "black"
-    }
-  })
+  addKotaLayer(map);
+  addPulauLayer(map);
 
   // Layer Raster
   map.addSource("spongebob", {
