09784f95fa8539bb6e1c7c5d668e84a797f1f7f6
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2026-07-13 21:04:26 +0700

    add cooperative gesture and disable double click zoom

--- webapp-maplibre/src/main.js
+++ webapp-maplibre/src/main.js
@@ -21,7 +21,8 @@ const map = new Map({
   style: 'https://demotiles.maplibre.org/globe.json',
   center: [106.83, -6.19],
   zoom: 1,
-  attributionControl: false
+  attributionControl: false,
+  cooperativeGestures: true
 });
 
 map.on("load", () => {
@@ -34,6 +35,8 @@ map.on("load", () => {
 map.on("click", "titik-kota", function(event){
   addKotaPopup(map, event);
 })
+
+map.doubleClickZoom.disable();
 
 
 // Controls setting
